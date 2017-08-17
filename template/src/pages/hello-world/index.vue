0a8b3c112d8c07a85c26aecd16d81f5e9303b2f1
walid <541280314@qq.com>
2017-08-17 15:06:03 +0800

    格式化code

--- template/src/pages/hello-world/index.vue
+++ template/src/pages/hello-world/index.vue
@@ -1,25 +1,29 @@
 <template>
-  <div>
-    <p>{{content}}</p>
-  </div>
+<div>
+  <p @click='onClick'>{{content}}</p>
+</div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        content: 'hello world'
-      }
+export default {
+  data() {
+    return {
+      content: 'hello world'
+    }
+  },
+  methods: {
+    onClick() {
+      console.log('onClick')
     }
   }
-
+}
 </script>
 
 <style scoped>
-  p {
-    font-size: 15px;
-    margin-top: 0;
-    color: #999;
-    text-align: center;
-  }
+p {
+  font-size: 15px;
+  margin-top: 0;
+  color: #999;
+  text-align: center;
+}
 </style>
